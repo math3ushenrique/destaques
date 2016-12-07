f186986f9f8b1ae62d3e8a26386ec9a61a849da4
Matheus Henrique <matheus.henrique@oi.com.br>
2016-12-07 13:01:07 -0200

    Enviando arquivos

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+	function exibirJanela(){
+			document.getElementById('fundo').style.display = "inline-block";
+			document.getElementById('fundo').style.transition = "0.5s ease-out";
+	}
+	function fecharJanela(){
+		document.getElementById('fundo').style.display = "none";
+	}
